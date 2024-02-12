46e750bf991cf7e764a15652c68b6299c8b2ce4c
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-02-12 00:04:19 -0300

    context + Frontend detalhesfilme

---
+++ filme/context.py
@@ -0,0 +1,10 @@
+
+from .models import Filme
+
+def lista_filmes_recentes(request):
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:10]
+    return {"lista_filmes_recentes": lista_filmes}
+    
+def lista_filmes_emalta(request):
+    lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:10]
+    return {"lista_filmes_emalta": lista_filmes}
