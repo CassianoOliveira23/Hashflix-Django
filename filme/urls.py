eed6fd91efb10542112406184c83320c9abab9d7
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-02-14 18:42:23 -0300

    editar senha

--- filme/urls.py
+++ filme/urls.py
@@ -1,5 +1,5 @@
 
-from django.urls import path, include
+from django.urls import path, reverse_lazy
 from .views import Homefilmes, Homepage, Detalhesfilme, Pesquisafilme, Paginaperfil, Criarconta
 from django.contrib.auth import views as auth_view
 
@@ -12,7 +12,9 @@ urlpatterns = [
     path('pesquisa/', Pesquisafilme.as_view(), name='pesquisafilme'),
     path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('editarperfil/', Paginaperfil.as_view(), name='editarperfil'),
+    path('editarperfil/<int:pk>', Paginaperfil.as_view(), name='editarperfil'),
     path('criarconta/', Criarconta.as_view(), name='criarconta'),
+    path('editarsenha/', auth_view.PasswordChangeView.as_view(template_name='editarperfil.html', 
+                                                              success_url=reverse_lazy('filme:homefilmes')), name="editarsenha"),
 ]
 
--- filme/views.py
+++ filme/views.py
@@ -1,9 +1,9 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect, reverse
-from .models import Filme,Usuario
+from .models import Filme, Usuario
 from .forms import CriarContaForm, FormHomepage
-from django.views.generic import TemplateView, ListView, DetailView, FormView
+from django.views.generic import TemplateView, ListView, DetailView, FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -67,8 +67,13 @@ class Pesquisafilme(LoginRequiredMixin, ListView):
             return None
         
 
-class Paginaperfil(LoginRequiredMixin, TemplateView):
+class Paginaperfil(LoginRequiredMixin, UpdateView):
     template_name = "editarperfil.html"
+    model = Usuario
+    fields = ['first_name', 'last_name', 'email']
+    
+    def get_success_url(self):
+        return reverse('filme:homefilmes')
     
 class Criarconta (FormView):
     template_name = "criarconta.html"
